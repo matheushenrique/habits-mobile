df9213459713a7d8770e9894ae3796d125b5e143
matheushenrique <matheus.henriquepr@gmail.com>
2023-02-03 14:37:51 -0300

    Create new screens and layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
 import { TouchableOpacity, View, Text } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
+import { useNavigation } from "@react-navigation/native";
 import Logo from "../assets/logo.svg";
 
 export function Header() {
+  const { navigate } = useNavigation();
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
       <TouchableOpacity
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
         activeOpacity={0.7}
+        onPress={() => navigate('new')}
       >
         <Feather
           name="plus"
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { View, Text, ScrollView } from "react-native";
 import { DAY_SIZE, HabitDay } from "../components/HabitDay";
 import { Header } from "../components/Header";
@@ -9,6 +10,8 @@ const minimunSummaryDatesSize = 18 * 5;
 const amountOfDaysToFill = minimunSummaryDatesSize - datesFromYearStart.length;
 
 export function Home() {
+  const {navigate} = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -31,6 +34,7 @@ export function Home() {
           {datesFromYearStart.map(date => (
             <HabitDay 
               key={date.toISOString()}
+              onPress={() => navigate('habit', {date: date.toISOString()})}
             />
           ))}
           {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill}).map((_, index) => (
